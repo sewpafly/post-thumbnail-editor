6f3f2daee546318173f00d9975b3c2fbb2a6a7b9
Brent Nef <sewpafly@gmail.com>
2011-03-15 17:38:03 -0700

    Fixed firefox/firebug loading issue - Closes #1

--- js/pte_admin_media.js
+++ js/pte_admin_media.js
@@ -1,16 +1,16 @@
 // log
-if (!window.console && !console.firebug)
-{
-    var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
-    "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
-
-    window.console = {};
-    for (var i = 0; i < names.length; ++i)
-        window.console[names[i]] = function() {}
-}
-if (!console) console = window.console;
-
 function log(obj){
+   if (!window.console || !console.firebug)
+   {
+      var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
+          "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
+
+      window.console = {};
+      for (var i = 0; i < names.length; ++i)
+         window.console[names[i]] = function() {}
+   }
+   if (!console) console = window.console;
+
    //console.log(obj);
    //alert(obj);
 }
