19c3526bab14d1a074a93c26a04aa3bbacaead4a
Brent <sewpafly@gmail.com>
2014-04-25 10:29:17 -0700

    fixed some js formatting

--- js/snippets/pte-fix-thickbox.js
+++ js/snippets/pte-fix-thickbox.js
@@ -3,15 +3,15 @@
 	tb_position = function() {
 		var tbWindow = $('#TB_window'), tbIframe = tbWindow.find('iframe');
 		if ( tbIframe.size() > 0 && tbIframe.get(0).src.match(/pte_ajax/) ) {
-         var W = $(window).width() - 100,
-               H = $(window).height() - 50,
-               adminbar_height = 0;
+			var W = $(window).width() - 100,
+			H = $(window).height() - 50,
+			adminbar_height = 0;
 
 			if ( $('body.admin-bar').length )
 				adminbar_height = 28;
 
-         // Set the default size
-         $('#TB_iframeContent').width(W).height(H - 75 - adminbar_height);
+			// Set the default size
+			$('#TB_iframeContent').width(W).height(H - 75 - adminbar_height);
 
 			tbWindow.css({'margin-left': '-' + parseInt((( W ) / 2),10) + 'px'});
 			if ( typeof document.body.style.maxWidth != 'undefined' )
